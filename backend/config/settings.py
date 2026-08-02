e314af4de72312e1b57dde2bece7e85f0c4eb35b
Khushi Kakade <khushikakade21@gmail.com>
2026-08-02 13:12:54 +0530

    fix: define STATICFILES_STORAGE explicitly to satisfy cloudinary_storage check in Django 5.x/6.x

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Media files (Uploaded by users like images)
 import os
